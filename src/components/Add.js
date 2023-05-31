66018b919a09c7762920a9607f3d0a70f342c7dc
Prasanna Nimbalkar <30629172+prasannanimbalkar@users.noreply.github.com>
2023-05-30 21:10:14 -0400

    Update Add.js

--- src/components/Add.js
+++ src/components/Add.js
@@ -11,7 +11,7 @@ export const Add = () => {
     setQuery(e.target.value);
 
     fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`
+      `https://api.themoviedb.org/3/search/movie?api_key=3274310e97e92b559fb09f4eeeae2906&language=en-US&page=1&include_adult=false&query=${e.target.value}`
     )
       .then((res) => res.json())
       .then((data) => {
